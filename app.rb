f338e210d108975e0b85c715aa7cd3ea6e500b73
Ashley Bland <ashleyjbland@gmail.com>
2019-07-11 20:53:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    @piglatinized_text = PigLatinizer.new(params[:user_text])
+
+    erb :results
+  end
 end
